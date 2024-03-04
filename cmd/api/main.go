ee9a5f09a66d71a3c5fafd23d147d1490f20f75b
lafetz <yafetyohaness@gmail.com>
2024-03-04 16:50:45 +0300

    add user route

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/lafetz/url-shortner/internal/adapters/primary/web"
+	"github.com/lafetz/url-shortner/internal/adapters/secondary/repository"
+	"github.com/lafetz/url-shortner/internal/core/services"
+)
+
+func main() {
+	db, err := repository.OpenDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	repo := repository.NewDb(db)
+	userService := services.NewUserService(repo)
+	urlService := services.NewUrlService(repo)
+	application := web.NewApp(*userService, *urlService)
+	application.Run()
+
+}
